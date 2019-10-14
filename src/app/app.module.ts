cfd2cddbc34078df4fcb24f17006e42161bcabf7
someone <someone@someplace.com>
2019-10-14 14:48:17 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AdMobFree } from '@ionic-native/admob-free/ngx';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { AdmobFreeService } from './service/admobfree.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    AdMobFree,
+    AdmobFreeService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdmobFreeService } from '../service/admobfree.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit {
 
-  constructor() {}
+  constructor(
+    private admobFreeService: AdmobFreeService,
+  ) {}
+
+  ngOnInit() {
+    this.admobFreeService.BannerAd();
+  }
+
+  showInterstitial(){
+    this.admobFreeService.InterstitialAd();
+  }
+  
+  showRewardVideo(){
+    this.admobFreeService.RewardVideoAd();
+  }
+
 
 }
